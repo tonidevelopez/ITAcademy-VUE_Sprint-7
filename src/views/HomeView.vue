23ee92f60f21f1ef1498aacb38e2e0e942a32033
tonidevelopez <101570134+tonidevelopez@users.noreply.github.com>
2023-02-15 15:20:00 +0100

    Exercici 1

---
+++ src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <p>Què vols fer?</p>
+    <p v-for="servei in serveis" :key="servei.id">
+      <input type="checkbox" :id="servei.id" :value="servei.preu" v-model="serveiChecked" @change="calcularTotal">
+      {{ servei.descripcio }}
+    </p>
+    <p>Preu: {{ preuTotal }}€</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      serveis: [
+        {
+          id: 1,
+          descripcio: "Una pàgina web (500 €)",
+          preu: 500,
+        },
+        {
+          id: 2,
+          descripcio: "Una consultoria SEO (300 €)",
+          preu: 300,
+        },
+        {
+          id: 3,
+          descripcio: "Una campanya de Google Ads (200 €)",
+          preu: 200,
+        }
+      ],
+      preuTotal: 0,
+      serveiChecked: []
+    }
+  },
+  methods: {
+    calcularTotal() {
+      this.preuTotal = 0
+      this.preuTotal += this.serveiChecked.reduce((a, b) => a + b, 0)
+
+      console.log(this.serveiChecked)
+    }
+  }
+}
+</script>
